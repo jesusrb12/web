b9d8ee7c6b063dedbe7f523859ecebe6fa9ec222
jesusrb12 <47484013+jesusrb12@users.noreply.github.com>
2019-03-07 19:37:14 -0500

    Base project

---
+++ src/app/app-routing.module.ts
@@ -0,0 +1,15 @@
+import { NgModule } from '@angular/core';
+import { Routes, RouterModule } from '@angular/router';
+
+export const routes: Routes = [
+  { path: '', redirectTo: 'mlogin', pathMatch: 'full'}, 
+  { path: 'mlogin', loadChildren: './login/login.module#LoginModule' },
+  { path: 'mestructura', loadChildren: './estructura/estructura.module#EstructuraModule' },
+  { path: '**', redirectTo: '' } // otherwise redirect to home
+];  
+
+@NgModule({
+  imports: [RouterModule.forRoot(routes)],
+  exports: [RouterModule]
+})
+export class AppRoutingModule { }
